56912c0119c49dc2a171009877eb2a9f4b3d7ecf
spyroid <soyroid@gmail.com>
2026-01-11 21:42:05 +0100

    .

--- src/aoc/y2020/d03/mod.rs
+++ src/aoc/y2020/d03/mod.rs
@@ -1,29 +1,22 @@
-use crate::gears::point::Point;
 use fun_time::fun_time;
 
 #[fun_time(give_back)]
 pub fn toboggan_trajectory(input: Vec<Box<str>>) -> (usize, usize) {
     let width = input[0].len();
 
-    let count_trees = |p: &Point| -> usize {
+    let count_trees = |p: &(usize, usize)| -> usize {
         input
             .iter()
-            .step_by(p.y)
+            .step_by(p.1)
             .enumerate()
-            .filter(|(i, row)| row.as_bytes()[i * p.x % width] == b'#')
+            .filter(|(i, row)| row.as_bytes()[i * p.0 % width] == b'#')
             .count()
     };
 
-    let slopes: Vec<_> = vec![
-        Point { x: 3, y: 1 },
-        Point { x: 1, y: 1 },
-        Point { x: 5, y: 1 },
-        Point { x: 7, y: 1 },
-        Point { x: 1, y: 2 },
-    ]
-    .iter()
-    .map(|p| count_trees(p))
-    .collect();
+    let slopes: Vec<_> = vec![(3, 1), (1, 1), (5, 1), (7, 1), (1, 2)]
+        .iter()
+        .map(|p| count_trees(p))
+        .collect();
 
     let part1 = slopes[0];
     let part2 = slopes.iter().fold(1usize, |acc, p| acc * *p);
